16763a362d1629c6f50e0dbad5d3d964902bc662
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2016-06-23 11:00:46 +0200

    Primitives: added support for specifying the broad argument type to primitive functions

--- src/aqua-primitives.adb
+++ src/aqua-primitives.adb
@@ -3,6 +3,7 @@ with Ada.Strings.Equal_Case_Insensitive;
 with Ada.Text_IO;
 
 with Aqua.IO;
+with Aqua.Objects.Arrays;
 
 package body Aqua.Primitives is
 
@@ -30,9 +31,10 @@ package body Aqua.Primitives is
 
    type Primitive_Function_Info is
       record
-         Name      : access String;
-         Arg_Count : Natural;
-         Handler   : access Handler_Interface'Class;
+         Name           : access String;
+         Arg_Count      : Natural;
+         Arg_Categories : access Argument_Category;
+         Handler        : access Handler_Interface'Class;
       end record;
 
    package Primitive_Function_Vectors is
@@ -53,7 +55,74 @@ package body Aqua.Primitives is
    is
       Info : Primitive_Function_Info
       renames Prim_Functions (Positive (Primitive));
+      Actuals : Array_Of_Words (1 .. Info.Arg_Count) :=
+                  (others => 0);
+      Current : Natural := 0;
+
+      procedure Check
+        (Index    : Positive;
+         Value    : Word;
+         Expected : String;
+         OK       : Boolean);
+
+      -----------
+      -- Check --
+      -----------
+
+      procedure Check
+        (Index    : Positive;
+         Value    : Word;
+         Expected : String;
+         OK       : Boolean)
+      is
+      begin
+         if not OK then
+            raise Aqua.Execution.Execution_Error
+            with "In call to " & Info.Name.all
+              & " at argument"
+              & Index'Img
+              & ": expected " & Expected
+              & " but found " & Context.Show (Value)
+              & " [" & Aqua.IO.Hex_Image (Value) & "]";
+         end if;
+      end Check;
+
    begin
+      for Arg of Arguments loop
+         Current := Current + 1;
+         if Current > Actuals'Last then
+            Ada.Text_IO.Put_Line
+              (Ada.Text_IO.Standard_Error,
+               "warning: too many arguments in call to "
+               & Info.Name.all);
+            exit;
+         else
+            Actuals (Current) := Arg;
+            case Info.Arg_Categories (Current) is
+               when Any =>
+                  null;
+               when Object_Argument =>
+                  Check (Current, Arg, "object", Is_External_Reference (Arg));
+               when Map_Argument =>
+                  Check (Current, Arg, "map",
+                         Is_External_Reference (Arg)
+                         and then Context.To_External_Object (Arg).all
+                         in Aqua.Objects.Object_Interface'Class);
+               when Array_Argument =>
+                  Check (Current, Arg, "array",
+                         Is_External_Reference (Arg)
+                         and then Context.To_External_Object (Arg).all
+                         in Aqua.Objects.Arrays.Root_Array_Type'Class);
+               when String_Argument =>
+                  Check (Current, Arg, "string",
+                         Is_String_Reference (Arg));
+               when Integer_Argument =>
+                  Check (Current, Arg, "integer",
+                         Is_Integer (Arg));
+            end case;
+         end if;
+      end loop;
+
       return Info.Handler.Handle (Context, Arguments);
    end Call_Primitive;
 
@@ -127,9 +196,25 @@ package body Aqua.Primitives is
       Argument_Count : Natural;
       Handler        : Primitive_Handler)
    is
+      Arg_Categories : constant Argument_Category (1 .. Argument_Count) :=
+                         (others => Any);
+   begin
+      New_Primitive_Function (Name, Arg_Categories, Handler);
+   end New_Primitive_Function;
+
+   ----------------------------
+   -- New_Primitive_Function --
+   ----------------------------
+
+   procedure New_Primitive_Function
+     (Name      : String;
+      Arguments : Argument_Category;
+      Handler   : Primitive_Handler)
+   is
    begin
       Prim_Functions.Append
-        ((new String'(Name), Argument_Count,
+        ((new String'(Name), Arguments'Length,
+         new Argument_Category'(Arguments),
          new Function_Handler_Interface'(Handler => Handler)));
    end New_Primitive_Function;
 
@@ -142,9 +227,25 @@ package body Aqua.Primitives is
       Argument_Count : Natural;
       Handler        : Handler_Interface'Class)
    is
+      Arg_Categories : constant Argument_Category (1 .. Argument_Count) :=
+                         (others => Any);
+   begin
+      New_Primitive_Handler (Name, Arg_Categories, Handler);
+   end New_Primitive_Handler;
+
+   ---------------------------
+   -- New_Primitive_Handler --
+   ---------------------------
+
+   procedure New_Primitive_Handler
+     (Name           : String;
+      Arguments      : Argument_Category;
+      Handler        : Handler_Interface'Class)
+   is
    begin
       Prim_Functions.Append
-        ((new String'(Name), Argument_Count,
+        ((new String'(Name), Arguments'Length,
+         new Argument_Category'(Arguments),
          new Handler_Interface'Class'(Handler)));
    end New_Primitive_Handler;
 
