25bb5bbb8c2d0d8ef8718b7d0850a3323f30c20e
Fraser Wilson <blancolioni@gmail.com>
2018-04-19 09:56:26 +0200

    Driver model update

--- src/aqua-drivers.ads
+++ src/aqua-drivers.ads
@@ -1,24 +1,99 @@
 package Aqua.Drivers is
 
-   type Aqua_Driver_Interface is interface;
+   type Driver_Register_Count is new Address range 0 .. 4096;
+   subtype Driver_Register_Range is Driver_Register_Count range 0 .. 4095;
 
-   type Aqua_Driver is access all Aqua_Driver_Interface'Class;
+   type Root_Aqua_Driver
+     (Last_Address : Driver_Register_Range)
+   is abstract tagged private;
 
-   function Address_Count (Driver : Aqua_Driver_Interface) return Address
-                           is abstract;
+   type Aqua_Driver is access all Root_Aqua_Driver'Class;
 
-   function Get_Octet
-     (Driver : Aqua_Driver_Interface;
-      Addr   : Address)
-      return Octet
-      is abstract;
-
-   procedure Set_Octet
-     (Driver : in out Aqua_Driver_Interface;
-      Addr   : Address;
-      Value  : Octet)
+   procedure Update
+     (Driver : in out Root_Aqua_Driver)
    is abstract;
 
+   function Get_Octet
+     (Driver : Root_Aqua_Driver'Class;
+      Addr   : Driver_Register_Range)
+      return Octet;
+
+   procedure Set_Octet
+     (Driver : in out Root_Aqua_Driver'Class;
+      Addr   : Driver_Register_Range;
+      Value  : Octet);
+
+   function Get_Word
+     (Driver : Root_Aqua_Driver'Class;
+      Addr   : Driver_Register_Range)
+      return Word;
+
+   procedure Set_Word
+     (Driver : in out Root_Aqua_Driver'Class;
+      Addr   : Driver_Register_Range;
+      Value  : Word);
+
+   function Changed_Octet
+     (Driver : Root_Aqua_Driver'Class;
+      Addr   : Driver_Register_Range)
+      return Boolean;
+
+   function Changed_Word
+     (Driver : Root_Aqua_Driver'Class;
+      Addr   : Driver_Register_Range)
+      return Boolean;
+
+   function Changed_Range
+     (Driver : Root_Aqua_Driver'Class;
+      Base   : Driver_Register_Range;
+      Bound  : Driver_Register_Count)
+      return Boolean;
+
+   procedure Clear_Changes
+     (Driver : in out Root_Aqua_Driver'Class);
+
    function Text_Writer return Aqua_Driver;
+
+private
+
+   type Device_Registers is
+     array (Driver_Register_Range range <>) of Octet;
+
+   type Register_Flags is
+     array (Driver_Register_Range range <>) of Boolean;
+
+   type Root_Aqua_Driver
+     (Last_Address : Driver_Register_Range)
+   is abstract tagged
+      record
+         Rs      : Device_Registers (0 .. Last_Address) := (others => 0);
+         Changed : Register_Flags (0 .. Last_Address)   := (others => False);
+      end record;
+
+   function Changed_Range
+     (Driver : Root_Aqua_Driver'Class;
+      Base   : Driver_Register_Range;
+      Bound  : Driver_Register_Count)
+      return Boolean
+   is (Bound > Base
+       and then (for some X in Base .. Bound - 1 => Driver.Changed (X)));
+
+   function Changed_Octet
+     (Driver : Root_Aqua_Driver'Class;
+      Addr   : Driver_Register_Range)
+      return Boolean
+   is (Driver.Changed_Range (Addr, Addr + 1));
+
+   function Changed_Word
+     (Driver : Root_Aqua_Driver'Class;
+      Addr   : Driver_Register_Range)
+      return Boolean
+   is (Driver.Changed_Range ((Addr / 4) * 4, (Addr / 4) * 4 + 4));
+
+   function Get_Octet
+     (Driver : Root_Aqua_Driver'Class;
+      Addr   : Driver_Register_Range)
+      return Octet
+   is (Driver.Rs (Addr));
 
 end Aqua.Drivers;
